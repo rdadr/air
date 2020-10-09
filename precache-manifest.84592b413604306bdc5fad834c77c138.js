2e45e122d3c6f2bac0f56ba24037b9d46214acaf
Sergii Sergiienko <66369690+rdadr@users.noreply.github.com>
2020-10-09 17:45:44 +0300

    Updates

---
+++ precache-manifest.84592b413604306bdc5fad834c77c138.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70dd48fb8f837f7395ed01244b8e0512",
+    "url": "/air/index.html"
+  },
+  {
+    "revision": "34f35fc967855c4fdeaa",
+    "url": "/air/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f6e7f4c3ca4db2261bb",
+    "url": "/air/static/js/2.5a8a203c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/air/static/js/2.5a8a203c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34f35fc967855c4fdeaa",
+    "url": "/air/static/js/main.53fe0e9a.chunk.js"
+  },
+  {
+    "revision": "df68bbc94b5ce2226899",
+    "url": "/air/static/js/runtime-main.3c781981.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/air/static/media/logo.5d5d9eef.svg"
+  }
+]);
